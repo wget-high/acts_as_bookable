4078a27c78af096eb346229f9df7e4b9cd8fe60a
captproton <carl@wdwhub.net>
2020-11-28 15:03:02 -0800

    CHANGE: updated specs from factory_girl to factory_bot

--- spec/factories/booker.rb
+++ spec/factories/booker.rb
@@ -1,5 +1,5 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :booker, class: 'Booker' do
-    name 'Booker name'
+    name { "Booker name" }
   end
 end
--- spec/factories/room.rb
+++ spec/factories/room.rb
@@ -1,7 +1,7 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :room, class: 'Room' do
-    name 'Room name'
-    capacity 4
+    name { "Room name" }
+    capacity { 4 }
     schedule {
       schedule = IceCube::Schedule.new(Date.today, duration: 1.day)
       schedule.add_recurrence_rule IceCube::Rule.daily
